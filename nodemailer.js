5631e7950b38e723856c29f7ad198acc68dad486
Omar Rasool <webdevromar@gmail.com>
2017-08-13 19:21:32 -0500

    you can now send test emails with nodemailer!

---
+++ nodemailer.js
@@ -0,0 +1,22 @@
+var nodemailer = require('nodemailer');
+
+let yourEmail = 'webdevromar@gmail.com';
+let theirEmail = 'omarmrasool@gmail.com';
+
+let emailSubject = 'Welcome!';
+
+let transporter = nodemailer.createTransport({
+    sendmail: true,
+    newline: 'unix',
+    path: '/usr/sbin/sendmail'
+});
+
+transporter.sendMail({
+    from: yourEmail,
+    to: theirEmail,
+    subject: emailSubject,
+    html: { path: 'dist/index.html' }
+}, (err, info) => {
+    console.log(info.envelope);
+    console.log(info.messageId);
+});
