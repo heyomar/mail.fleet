747a39087a38fc0952726f1178450dfbccaa5f32
heyomar <odyenri@gmail.com>
2022-07-07 22:25:57 -0400

    format nodemailer js

--- nodemailer.js
+++ nodemailer.js
@@ -1,22 +1,25 @@
-var nodemailer = require('nodemailer');
+var nodemailer = require('nodemailer')
 
-let yourEmail = 'webdevromar@gmail.com';
-let theirEmail = 'omarmrasool@gmail.com';
+let yourEmail = 'testemail@gmail.com'
+let theirEmail = 'testemail@gmail.com'
 
-let emailSubject = 'Welcome!';
+let emailSubject = 'Welcome!'
 
 let transporter = nodemailer.createTransport({
-    sendmail: true,
-    newline: 'unix',
-    path: '/usr/sbin/sendmail'
-});
+	sendmail: true,
+	newline: 'unix',
+	path: '/usr/sbin/sendmail',
+})
 
-transporter.sendMail({
-    from: yourEmail,
-    to: theirEmail,
-    subject: emailSubject,
-    html: { path: 'dist/index.html' }
-}, (err, info) => {
-    console.log(info.envelope);
-    console.log(info.messageId);
-});
+transporter.sendMail(
+	{
+		from: yourEmail,
+		to: theirEmail,
+		subject: emailSubject,
+		html: { path: 'dist/index.html' },
+	},
+	(err, info) => {
+		console.log(info.envelope)
+		console.log(info.messageId)
+	}
+)
